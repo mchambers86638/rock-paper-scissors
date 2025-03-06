e35175b089576b75c6cb05edcdcf9941a406e921
mchambers86638 <mchambers86638@student.greendot.org>
2025-03-05 18:48:27 -0800

    Update pxt.json, main.blocks, main.ts, pxt.json, _history

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let hand = 0
 input.onGesture(Gesture.Shake, function () {
-    music.play(music.tonePlayable(440, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("G B A G C5 B A B ", 120), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
     hand = randint(1, 3)
     if (0 == 0) {
         basic.showIcon(IconNames.SmallSquare)
@@ -10,12 +11,14 @@ input.onGesture(Gesture.Shake, function () {
         hand = randint(1, 3)
     } else if (false) {
         hand = randint(1, 3)
+        basic.showIcon(IconNames.SmallSquare)
         basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.Scissors)
     } else if (false) {
         hand = randint(1, 3)
     } else if (false) {
-        basic.showIcon(IconNames.Scissors)
-    } else {
+        basic.showIcon(IconNames.SmallSquare)
+        basic.showIcon(IconNames.Square)
         basic.showIcon(IconNames.Scissors)
     }
 })
